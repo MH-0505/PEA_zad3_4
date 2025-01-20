f3a29816ed79e7b275695fa5fcd13b05b17a5f1c
michał <272980@student.pwr.edu>
2025-01-20 20:48:57 +0100

    4

--- simulated_annealing/main_sa.cpp
+++ simulated_annealing/main_sa.cpp
@@ -18,8 +18,9 @@ int main() {
     std::map<std::string, std::string> configuration = ConfigManager::load_configuration("config_sa.txt");
     std::vector<std::string> filenames = ConfigManager::parse_filenames(configuration);
     AdjMatrix graph;
-    std::fstream output;
+    std::fstream output, output2;
     output.open("output_sa.csv", std::ios::app);
+
     auto now = std::chrono::system_clock::now();
     std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
     std::tm* local_time = std::localtime(&now_time_t);
@@ -41,10 +42,13 @@ int main() {
 
         std::cout << "\n\nROZPOCZETO BADANIE\nMetoda: simulated annealing\nNazwa pliku: " << filename << "\nWynik optymalny: " << graph.tsp_optimal_weight;
         output.open("output_sa.csv", std::ios::app);
+        output2.open("output_sa_2.csv", std::ios::app);
         output << "\n\nplik:;" << filename << "\n"
                << "wynik optymalny:;" << graph.tsp_optimal_weight << "\n"
                << "liczba wierzcholkow:;" << graph.vertex_count << "\n"
-               << "iteracja;czas [us];wynik;blad bezwzgledny;blad wzgledny;sciezka\n";
+               << "iteracja;czas [ms];wynik;blad bezwzgledny;blad wzgledny;sciezka\n";
+
+        output2 << std::put_time(local_time, "%H:%M:%S") << ";";
 
         long long avg_time = 0;
         int error_sum = 0;
@@ -64,7 +68,7 @@ int main() {
                                                              std::stoi(configuration["random_start_path"]));
 
             auto end_time = std::chrono::high_resolution_clock::now();
-            auto time = std::chrono::duration_cast<std::chrono::microseconds >(end_time-start_time).count();
+            auto time = std::chrono::duration_cast<std::chrono::milliseconds >(end_time-start_time).count();
 
             int absolute_error = std::abs(graph.tsp_optimal_weight - results[0]);
             double relative_error = static_cast<double>(absolute_error)/ graph.tsp_optimal_weight * 100.0;
@@ -75,16 +79,18 @@ int main() {
             ss << std::fixed << std::setprecision(2) << relative_error;
             std::string error_string = ss.str() + '%';
 
-            std::cout << "\n\nIteracja: " << i << "\nCzas realizacji: " << time << " us\nWynik algorytmu: " << results[0]
+            std::cout << "\n\nIteracja: " << i << "\nCzas realizacji: " << time << " ms\nWynik algorytmu: " << results[0]
                       << "\nBlad bezwzgledny: " << absolute_error << "\nBlad wzgledny: " << error_string << "\nWyznaczona sciezka: " << path_to_string(results);
             output << i << ";" << time << ";" << results[0] << ";" << absolute_error << ";" <<relative_error << ";" << path_to_string(results)  << "\n";
         }
         avg_time = avg_time/iterations;
         double ua = static_cast<double>(error_sum) / iterations;
 
-        output << "sredni czas [us];blad bezwzgledny;blad bezwzgledny x2;blad wzgledny [%]\n";
-        output << avg_time << ";" << ua << ";" << ua*2 << ";" << 2*ua/graph.tsp_optimal_weight * 100.0 << "\n";
+        output << "sredni czas [ms];blad bezwzgledny;blad bezwzgledny x2;blad wzgledny [%]\n";
+        output << avg_time << ";" << ua << ";" << ua*2 << ";" << 2*ua/graph.tsp_optimal_weight << "\n";
+        output2 << filename << ";" << graph.vertex_count << ";" << avg_time << ";" << ua << ";" << ua*2 << ";" << 2*ua/graph.tsp_optimal_weight << "\n";
         output.close();
+        output2.close();
         graph.deleteMatrix();
     }
 
